2470641ee9eea1f6ea1b88d2e7783f8f5eb63d15
Kessir wassim <53373646+Muk4g3n@users.noreply.github.com>
2024-02-14 20:25:55 +0100

    added splash scrapy to parse dynamic pages

--- webcrawler/webcrawler/spiders/crawling_spider.py
+++ webcrawler/webcrawler/spiders/crawling_spider.py
@@ -1,18 +1,40 @@
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
-
+from scrapy_splash import SplashRequest
 
 
 class CrawlingSpider(CrawlSpider):
     name = "sathyacrawler"
     allowed_domains = ["sathyasai.org"]
-    start_urls = ["https://saispeaks.sathyasai.org/discourses/?collection=Sri%20Sathya%20Sai%20Speaks%2C%20Vol%204%20%281964%29"]
+    start_urls = ["https://www.sathyasai.org/resources/ebooks/sathya-sai-speaks"]
 
 
-    # rules = (
-    #     Rule(
-    #         LinkExtractor(allow='discourses/.*'),
-    #         # callback='parse_page',
-    #         follow=True
-    #     ),
-    # )
+    # def start_requests(self):
+    #     for url in self.start_urls:
+    #         yield SplashRequest(url, args={'wait': 2})
+
+    
+
+    rules = (
+        Rule(
+            LinkExtractor(allow='discourses/.*'),
+            callback='parse',
+            # process_request='splash_request',
+        ),
+    )
+
+
+    def splash_request(self, request):
+        return SplashRequest(
+            request.url,
+            callback=request.callback,
+            endpoint='render.html',
+            args={'wait': 3.5},
+        )
+    def parse(self,response):
+        discourse_listings = response.css('.discourse-listings')
+        for discourse_listing in discourse_listings:
+            yield {
+                "title":discourse_listing.css('.discourse-listing > .title::text').get(),
+                "content":discourse_listing.css('.discourse-listing > .content::text').get(),
+            }
