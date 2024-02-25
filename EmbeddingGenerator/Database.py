e16e5ea220ac068b13f5a98102ef3e95346d86d6
Kessir wassim <53373646+Muk4g3n@users.noreply.github.com>
2024-02-25 17:35:15 +0100

    cleaned the code

--- EmbeddingGenerator/Database.py
+++ EmbeddingGenerator/Database.py
@@ -25,17 +25,20 @@ with open('..\Web scraper\data.json', 'r') as file:
     # Load the JSON data
     data = json.load(file)
 
+# print(data)
 
 
 def model(text):
     return openai.embeddings.create(input = [text], model="text-embedding-3-small").data[0].embedding
 
 # create the embedding for the content and insert inside the database 
-for d in data:
-    text = d['Content'].replace("\n", " ")
-    # insertEmbedding(collection=collection,model=model,document=d)
-    d['content_embedding'] = model(text)
-    collection.insert_one(d)
+
+def embeddingGenerator():
+    for d in data:
+        text = d['Content'].replace("\n", " ")
+        # insertEmbedding(collection=collection,model=model,document=d)
+        d['content_embedding'] = model(text)
+        collection.insert_one(d)
 
 ### testing the search functionality
 # query = "what is prayer ?"
