0c6bca153bd9c79cc0cfe1cc2950578aad2ddbfb
castrombithisamm <castro.mbithii@gmail.com>
2024-03-21 13:02:58 +0300

    add database configurations to database.py

--- EmbeddingGenerator/Database.py
+++ EmbeddingGenerator/Database.py
@@ -5,20 +5,28 @@ import json
 import openai
 from openai import OpenAI
 # from sentence_transformers import SentenceTransformer
-from utils import generateEmbeddings,search,insertEmbedding
-
+from utils import generateEmbeddings, search, insertEmbedding
 
 
 load_dotenv()
 
 openai.api_key = os.getenv("OPEN_AI")
 
-client = pymongo.MongoClient(os.getenv("Mongo_uri"))
+# client = pymongo.MongoClient(os.getenv("Mongo_uri"))
+
+MONGO_HOST = os.getenv('MONGO_HOST')
+MONGO_PORT = int(os.getenv('MONGO_PORT'))  # Convert port to integer
+MONGO_AUTH_SOURCE = os.getenv('MONGO_AUTH_SOURCE')
+MONGO_USERNAME = os.getenv('MONGO_USERNAME')
+MONGO_PASSWORD = os.getenv('MONGO_PASSWORD')
+COLLECTION_NAME = 'text'  # Update with your MongoDB collection name
+
+
+# Use these variables to connect to MongoDB
+client = pymongo.MongoClient(host=MONGO_HOST, port=MONGO_PORT, authSource=MONGO_AUTH_SOURCE,
+                             username=MONGO_USERNAME, password=MONGO_PASSWORD)
 db = client.saibabasayings
 collection = db.text
-
-
-
 
 
 with open('..\Web scraper\data.json', 'r') as file:
@@ -29,9 +37,10 @@ with open('..\Web scraper\data.json', 'r') as file:
 
 
 def model(text):
-    return openai.embeddings.create(input = [text], model="text-embedding-3-small").data[0].embedding
+    return openai.embeddings.create(input=[text], model="text-embedding-3-small").data[0].embedding
 
-# create the embedding for the content and insert inside the database 
+# create the embedding for the content and insert inside the database
+
 
 def embeddingGenerator():
     for d in data:
@@ -40,6 +49,6 @@ def embeddingGenerator():
         d['content_embedding'] = model(text)
         collection.insert_one(d)
 
-### testing the search functionality
+# testing the search functionality
 # query = "what is prayer ?"
 # search(model(query),collection)
