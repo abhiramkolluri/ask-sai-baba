431cf6ae6dace733b6d190a8db4f6b21e47f9762
castrombithisamm <castro.mbithii@gmail.com>
2024-03-22 10:47:36 +0300

    /api/primarysource/query endpoint logic and request handling

--- backend/app.py
+++ backend/app.py
@@ -31,6 +31,8 @@ db = client.saibabasayings
 collection = db.text
 
 # embedding generator
+
+
 def model(text):
 
     return openai.embeddings.create(input=[text], model="text-embedding-3-small").data[0].embedding
@@ -52,6 +54,26 @@ def search_endpoint():
     query = request.form['query']
     results = search(model(query), collection)
     return jsonify(results)
+
+
+# Endpoint for querying Sathya Sai Baba's teachings
+@app.route('/api/primarysource/query', methods=['POST'])
+def query_sai_baba():
+    data = request.json
+    query = data.get('query')
+
+    # Perform query on MongoDB collection
+    result = collection.find_one(
+        {'title': {'$regex': query, '$options': 'i'}},
+        # {'_id': 0}  # Exclude _id field from the result
+    )
+
+    if result:
+        # Convert ObjectId to string
+        result['_id'] = str(result['_id'])
+        return jsonify({'response': result}), 200
+    else:
+        return jsonify({'message': 'No results found.'}), 404
 
 
 if __name__ == "__main__":
