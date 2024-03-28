4ae97b9b2b6632b07cc75e631cffbc5ebb97000e
castrombithisamm <castro.mbithii@gmail.com>
2024-03-28 08:51:49 +0300

    use jwt tokens for auth

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,27 @@
 from flask import Flask, url_for, render_template, request, redirect, jsonify
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
 from dotenv import load_dotenv
 import os
+import binascii
 from pymongo import MongoClient
 import pymongo
 import openai
 import json
 from utils import search
 
+# Initialize Flask app
+app = Flask(__name__)
+
+
 # loading the env variables
 load_dotenv()
+
+
+# Generate a random hex string with 32 bytes
+secret_key = binascii.hexlify(os.urandom(32)).decode()
+
+print("Generated JWT Secret Key:", secret_key)
+
 
 # setting up open ai and mongodb
 openai.api_key = os.getenv("OPEN_AI")
@@ -23,6 +36,12 @@ MONGO_USERNAME = os.getenv('MONGO_USERNAME')
 MONGO_PASSWORD = os.getenv('MONGO_PASSWORD')
 
 
+app.config['JWT_SECRET_KEY'] = secret_key
+# os.getenv(
+#     'JWT_SECRET_KEY')  # Use your own secret key
+# app.config['JWT_ACCESS_TOKEN_EXPIRES'] = 3600  # 1 hour
+
+
 # Use these variables to connect to MongoDB
 client = MongoClient(host=MONGO_HOST, port=MONGO_PORT, authSource=MONGO_AUTH_SOURCE,
                      username=MONGO_USERNAME, password=MONGO_PASSWORD)
@@ -30,15 +49,14 @@ client = MongoClient(host=MONGO_HOST, port=MONGO_PORT, authSource=MONGO_AUTH_SOU
 db = client.saibabasayings
 collection = db.text
 
+
 # embedding generator
-
-
 def model(text):
 
     return openai.embeddings.create(input=[text], model="text-embedding-3-small").data[0].embedding
 
 
-app = Flask(__name__)
+jwt = JWTManager(app)
 
 
 @app.route('/', methods=['GET'])
@@ -58,6 +76,7 @@ def search_endpoint():
 
 # Endpoint for querying Sathya Sai Baba's teachings
 @app.route('/api/primarysource/query', methods=['POST'])
+@jwt_required()
 def query_sai_baba():
     data = request.json
     query = data.get('query')
@@ -74,24 +93,12 @@ def query_sai_baba():
         return jsonify({'response': result}), 200
     else:
         return jsonify({'message': 'No results found.'}), 404
-# Define a function to insert a user document into the "users" collection
-
-
-def create_user(first_name, last_name, email, password):
-    user = db['users']
-    user = {
-        'first_name': first_name,
-        'last_name': last_name,
-        'email': email,
-        'password': password
-    }
-    db.user.insert_one(user)
 
 
 @app.route('/register', methods=['POST'])
 def register():
     email = request.form.get('email')
-    if db.user.find_one({'email': email}):
+    if db.users.find_one({'email': email}):
         return jsonify({'message': 'User already exists'}), 409
     else:
         user_data = {
@@ -100,7 +107,26 @@ def register():
             'email': email,
             'password': request.form.get('password')
         }
-        db.user.insert_one(user_data)
+        db.users.insert_one(user_data)
         return jsonify({'message': 'User registered successfully'}), 201
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    if request.is_json:
+        email = request.json.get('email')
+        password = request.json.get('password')
+
+    else:
+        email = request.form.get('email')
+        password = request.form.get('password')
+    user = db.users.find_one({'email': email, 'password': password})
+    if user:
+        access_token = create_access_token(identity=email)
+        return jsonify({'access_token': access_token}), 200
+    else:
+        return jsonify({'message': 'Invalid credentials'}), 401
+
+
 if __name__ == "__main__":
     app.run(debug=True)
