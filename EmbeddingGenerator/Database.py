661dde77f92b1029eef1fe1785fab57886647909
castrombithisamm <castro.mbithii@gmail.com>
2024-04-17 19:03:14 +0300

    improve database.py for generated embeddings insertion

--- EmbeddingGenerator/Database.py
+++ EmbeddingGenerator/Database.py
@@ -1,30 +1,26 @@
+import uuid
 from dotenv import load_dotenv
 import os
+from pymongo import MongoClient
+from decouple import config
+import configparser
 import pymongo
 import json
-import openai
 from openai import OpenAI
-# from sentence_transformers import SentenceTransformer
+from sentence_transformers import SentenceTransformer
 from utils import generateEmbeddings, search, insertEmbedding
 
 
+
 load_dotenv()
+config = configparser.ConfigParser()
+config.read('openai.ini')
 
-openai.api_key = os.getenv("OPEN_AI")
-
-# client = pymongo.MongoClient(os.getenv("Mongo_uri"))
-
-MONGO_HOST = os.getenv('MONGO_HOST')
-MONGO_PORT = int(os.getenv('MONGO_PORT'))  # Convert port to integer
-MONGO_AUTH_SOURCE = os.getenv('MONGO_AUTH_SOURCE')
-MONGO_USERNAME = os.getenv('MONGO_USERNAME')
-MONGO_PASSWORD = os.getenv('MONGO_PASSWORD')
-COLLECTION_NAME = 'text'  # Update with your MongoDB collection name
+# openai.api_key = os.getenv("OPEN_AI")
+client = MongoClient(os.getenv("MONGO_URI"))
+openai_client = OpenAI(api_key=config['OpenAI']['api_key'])
 
 
-# Use these variables to connect to MongoDB
-client = pymongo.MongoClient(host=MONGO_HOST, port=MONGO_PORT, authSource=MONGO_AUTH_SOURCE,
-                             username=MONGO_USERNAME, password=MONGO_PASSWORD)
 db = client.saibabasayings
 collection = db.text
 
@@ -34,12 +30,17 @@ with open('../../ask-sai-baba/Web scraper/data.json', 'r') as file:
     # Load the JSON data
     data = json.load(file)
     for item in data:
-        if not collection.find_one(item):
+        # Ensure each document has an _id field
+        if '_id' not in item:
+            # Generate a unique identifier if _id is missing
+            item['_id'] = str(uuid.uuid4())
+        # Check if the document already exists in the collection
+        if not collection.find_one({'_id': item['_id']}):
             collection.insert_one(item)
 
 
 def model(text):
-    return openai.embeddings.create(input=[text], model="text-embedding-3-small").data[0].embedding
+    return openai_client.embeddings.create(input=[text], model="text-embedding-3-small").data[0].embedding
 
 
 # create the embedding for the content and insert inside the database
