8dabc0519e023fc6ad5cb1f926036b9f1a6aca49
castrombithisamm <castro.mbithii@gmail.com>
2024-04-21 16:28:08 +0300

    make improvements on spider

--- webcrawler/webcrawler/spiders/crawling_spider.py
+++ webcrawler/webcrawler/spiders/crawling_spider.py
@@ -1,49 +1,43 @@
-# import scrapy
-from scrapy.spiders import CrawlSpider, Rule
-from scrapy.linkextractors import LinkExtractor
+import scrapy
 from scrapy_splash import SplashRequest
 
 
-class CrawlingSpider(CrawlSpider):
-    name = "sathyacrawler"
-    allowed_domains = ["sathyasai.org"]
-    visited_urls_file = 'visited_urls.txt' 
-    # start_urls = ["https://www.sathyasai.org/resources/ebooks/sathya-sai-speaks"]
-    
-    start_urls = ["https://saispeaks.sathyasai.org/discourses/?collection=Sri%20Sathya%20Sai%20Speaks%2C%20Vol%2043%20%282010%29"]
+class CrawlingSpider(scrapy.Spider):
+  name = "sathyacrawler"
+  allowed_domains = ["saispeaks.sathyasai.org"]
+  visited_urls_file = 'visited_urls.txt'
+  start_url = "https://saispeaks.sathyasai.org/discourses/?collection=Sri%20Sathya%20Sai%20Speaks%2C%20Vol%2035%20%282002%29&page={}"
 
-    def start_requests(self):
-        for url in self.start_urls:
-            yield SplashRequest(url,self.parse,endpoint='render.html',args={'wait':5})
-        # url = "https://saispeaks.sathyasai.org/discourses/?collection=Sri%20Sathya%20Sai%20Speaks%2C%20Vol%2043%20%282010%29"
-        # yield scrapy.Request(url=url,meta={'playwright':True})
+  def start_requests(self):
+    yield SplashRequest(self.start_url, self.parse, endpoint='render.html', args={'wait': 5})
 
-    
+  def parse(self, response):
+    self.logger.info(f"Processing page: {response.url}")
 
-    # rules = (
-    #     Rule(
-    #         LinkExtractor(allow='discourses/.*'),
-    #         # callback='parse',
-    #         # process_request='splash_request',
-    #     ),
-    # )
+    # Extract data from the current page
+    discourse_listings = response.css('.discourse-listing')
+    for discourse_listing in discourse_listings:
+      yield {
+          "title": discourse_listing.css('.title a::text').get(),
+          "content": discourse_listing.css('.content::text').get(),
+      }
 
-    # def splash_request(self, request):
-    #     return SplashRequest(
-    #         request.url,
-    #         callback=request.callback,
-    #         endpoint='render.html',
-    #         args={'wait': 3.5},
-    #     )
+      # Extract PDF URLs and yield requests to download them
+      pdf_urls = response.css('a[href$=".pdf"]::attr(href)').getall()
+      for pdf_url in pdf_urls:
+        yield scrapy.Request(pdf_url, callback=self.save_pdf)
 
-    # def parse(self, response):
-    #     # url = 'http://www.licitor.com/ventes-judiciaires-immobilieres/tgi-fontainebleau/mercredi-15-juin-2016.html'
-    #     yield SplashRequest(url=response.url, callback=self.parse_item, args={'wait': 0.5})
+    # Follow pagination links
+    next_page_link = response.css(
+        '.pagination-links .next a::attr(href)').get()
+    if next_page_link:
+      self.logger.info(f"Following next page link: {next_page_link}")
+      yield SplashRequest(response.urljoin(next_page_link), self.parse, endpoint='render.html', args={'wait': 5})
+    else:
+      self.logger.info("No next page link found.")
 
-    def parse(self,response):
-        discourse_listings = response.css('.discourse-listings')
-        for discourse_listing in discourse_listings:
-            yield {
-                "title":discourse_listing.css('.discourse-listing > .title::text').get(),
-                "content":discourse_listing.css('.discourse-listing > .content::text').get(),
-            }
+  def save_pdf(self, response):
+    # Save PDF files to a directory
+    filename = response.url.split('/')[-1]
+    with open(filename, 'wb') as f:
+      f.write(response.body)
