048db6e5b56da04f3fb797425f0c79331d5a11e8
castrombithisamm <castro.mbithii@gmail.com>
2024-04-30 21:34:49 +0300

    ensure data integrity by removing any empty data point/row

--- EmbeddingGenerator/load_dataset.py
+++ EmbeddingGenerator/load_dataset.py
@@ -53,3 +53,61 @@ except Exception as e:
 # Display the first 5 rows of the DataFrame
 print(dataset_df.head(5))
 
+
+def get_embedding(text):
+    """Generate an embedding for the given text using OpenAI's API."""
+
+    # Check for valid input
+    if not text or not isinstance(text, str):
+        return None
+
+    try:
+        # Call OpenAI API to get the embedding
+        embedding = openai_client.embeddings.create(
+            input=text, model="text-embedding-3-large").data[0].embedding
+        return embedding
+    except Exception as e:
+        logging.error(f"Error generating embedding: {e}")
+        return None
+
+
+# Remove the paragraph_embedding_optimised from each data point in the dataset as we are going to create new embeddings with the new OpenAI embedding Model "text-embedding-3-large"
+try:
+    dataset_df = dataset_df.dropna(subset=['content'])
+    print("\\nNumber of missing values in each column after removal:")
+    print(dataset_df.isnull().sum())
+    print("Dropping column 'paragraph_embedding_optimised'...")
+    dataset_df = dataset_df.drop(columns=['paragraph_embedding_optimised'])
+    print("Column 'paragraph_embedding_optimised' dropped.")
+except Exception as e:
+    logging.error(f"Error dropping column: {e}")
+
+
+# Generate embeddings for the 'content' column
+try:
+    print("Generating embeddings...")
+    dataset_df["paragraph_embedding"] = dataset_df['content'].apply(
+        get_embedding)
+    print("Embeddings generated.")
+except Exception as e:
+    logging.error(f"Error generating embeddings: {e}")
+
+
+# Convert DataFrame to dictionary
+try:
+    print("Converting DataFrame to dictionary...")
+    documents = dataset_df.to_dict('records')
+    print("DataFrame converted to dictionary.")
+except Exception as e:
+    logging.error(f"Error converting DataFrame to dictionary: {e}")
+    print(f"Error converting DataFrame to dictionary: {e}")
+
+# Insert data into MongoDB
+try:
+    print("Inserting data into MongoDB...")
+    collection.insert_many(documents)
+    print("Data insertion into MongoDB completed.")
+except Exception as e:
+    logging.error(f"Error inserting data into MongoDB: {e}")
+    print(f"Error inserting data into MongoDB: {e}")
+
