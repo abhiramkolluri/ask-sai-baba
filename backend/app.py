bc13f222b733d5592a027b43e224350d6444f4ca
Yeshwanth Raja <yesh_rocks@hotmail.com>
2024-12-18 22:10:30 -0600

    Updated requirements with bcrypt

--- backend/app.py
+++ backend/app.py
@@ -4,10 +4,10 @@ from flask_jwt_extended import JWTManager, create_access_token
 from dotenv import load_dotenv
 import configparser
 import os
+import bcrypt
 from datetime import datetime
 
 import binascii
-from flask_bcrypt import Bcrypt
 from pymongo import MongoClient
 from openai import OpenAI
 from utils import handle_user_query, search_browse, get_full_article, model
@@ -16,7 +16,7 @@ from utils import handle_user_query, search_browse, get_full_article, model
 app = Flask(__name__)
 
 # CORS configuration
-cors = CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
+cors = CORS(app)
 
 # loading the env variables
 load_dotenv()
@@ -56,7 +56,7 @@ def store_user_query(query_text, query_embedding):
         'timestamp': timestamp
     }
     # Insert query data into MongoDB collection
-    db.user_queries.insert_one(query_data)
+    # db.user_queries.insert_one(query_data)
 
 
 @app.route('/search', methods=['POST'])
@@ -67,7 +67,7 @@ def search_endpoint():
             # Generate query embedding
             query_embedding = model(query)
             # Store user query and embedding in the collection
-            store_user_query(query, query_embedding)
+            # store_user_query(query, query_embedding)
             # Proceed with search and return results
             results = search_browse(query_embedding, article_collection)
             return jsonify(results)
@@ -109,24 +109,33 @@ def get_article(id):
 
 @app.route('/register', methods=['POST'])
 def register():
+    data = request.json
+    if 'email' not in data:
+        return jsonify({"error": "Email is required"}), 400
+    if 'password' not in data:
+        return jsonify({"error": "Password is required"}), 400
+
     email = request.form.get('email')
     if db.users.find_one({'email': email}):
         return jsonify({'message': 'User already exists'}), 409
     else:
+        try:
+            hashed_password = bcrypt.hashpw(
+                request.form.get('password').encode('utf-8'),
+                bcrypt.gensalt()
+            )
 
-        hashed_password = bcrypt.hashpw(
-            request.form.get('password').encode('utf-8'),
-            bcrypt.gensalt()
-        )  
+            user_data = {
+                'first_name': request.form.get('first_name'),
+                'last_name': request.form.get('last_name'),
+                'email': email,
+                'password': request.form.get('password')
 
-        user_data = {
-            'first_name': request.form.get('first_name'),
-            'last_name': request.form.get('last_name'),
-            'email': email,
-            'password': request.form.get('password')
-            
-        }
-        db.users.insert_one(user_data)
+            }
+            db.users.insert_one(user_data)
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            return jsonify({'message': 'Registration unsuccessful'}), 400
         return jsonify({'message': 'User registered successfully'}), 201
 
 
